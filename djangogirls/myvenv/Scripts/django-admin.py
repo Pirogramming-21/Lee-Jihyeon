2515729f8360468fbeb34b99682d67094624751f
Jihyeon Lee <ljh130334@naver.com>
2024-07-09 22:48:02 +0900

    나의 첫 번째 Django 프로젝트!

--- djangogirls/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\ljh13\OneDrive\Desktop\피로그래밍\piro21\Lee-Jihyeon\djangogirls\myvenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
